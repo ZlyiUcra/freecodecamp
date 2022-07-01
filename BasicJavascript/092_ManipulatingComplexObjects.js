ed6b237523a00a9e062e735dd0535507149b3fc5
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-01 22:56:39 +0100

    Manipulating Complex Objects

---
+++ BasicJavascript/092_ManipulatingComplexObjects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+   {
+    "artist": "ABBA Teens",
+    "title": "Mamma mia",
+    "release_year": 1997,
+    "formats": [
+      "CD",
+      "Cassete",
+      "LP"
+    ],
+    "gold": false
+  }
+];
