c2f354caa3da14fa56fcb51d227e67b441fc0eec
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-07 00:45:53 +0100

    Split a String into an Array Using the split Method

---
+++ 08_FunctionalProgramming/19_SplitAStringIntoAnArrayUsingTheSplitMethod.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+  const result = str.split(/\W/);
+  return result;
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
